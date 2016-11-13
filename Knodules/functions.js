008ad7b3a352e0552aa84c295607a63f606f4e9c
Mark Puchala II <markpuchalaii@gmail.com>
2016-11-12 18:50:33 -0600

    A knowledge-plotter!
    The first steps in a program with which I hope to plot out everything I know & learn.

---
+++ Knodules/functions.js
@@ -0,0 +1,9 @@
+function test(){
+  document.getElementById("centered").innerHTML  += "test successful";
+}
+
+function createCircle(){
+  document.getElementById("centered").innerHTML  +=
+  "<div draggable='true' class='circle default'></div>";
+}
+/
