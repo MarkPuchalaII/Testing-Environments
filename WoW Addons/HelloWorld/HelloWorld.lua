098d6acf39cf4f8d75c58a69c7ed9f4cf96c9b59
Mark Puchala II <markpuchalaii@gmail.com>
2016-11-25 19:04:45 -0600

    Updates with a few notes
    Had only gotten so far as a few comments to note what's what.
    More work later.

--- WoW Addons/HelloWorld/HelloWorld.lua
+++ WoW Addons/HelloWorld/HelloWorld.lua
@@ -7,6 +7,8 @@ SlashCmdList["HELLO_WORLD_ADD"] = function(msg)
   local, id, text = msg:match("(%S+)%s+(.+)")
   if id and text then
     HelloWorld_Text[id:lower()] = text
+  else
+    print("Usage: /hwadd <id> <text>")
   end
 end
 
@@ -15,6 +17,8 @@ SLASH_HELLO_WORLD_SHOW2 = "/helloworldshow" -- an alias for /hwshow
 SlashCmdList["HELLO_WORLD_SHOW"] = function(msg)
   local text = HelloWorld_Text[msg:lower()]
   if text then
-    SendChatMessage(text, channel)
-  end
-end
+    SendChatMessage(text, channel) -- function SeNdChatMessage(msg, channel, language, target)
+  end -- msg      = "Your message"
+end   -- channel  = SAY(default), YELL, EMOTE, WHISPER, PARTY, RAID, BATTLEGROUND, RAID WARNING, GUILD, OFFICER
+      -- language = Language to use (nil uses character's default language)
+      -- target   = only needed for channel = WHISPER
